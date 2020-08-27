a949ff8bb8156e6974543d69b8aa52bb1e430e4c
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-27 01:33:28 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,15 @@
-
-def_t(["Tim", "Tom", "Jim"]) do|name|
-if name.start_with?("T")
-  puts "Hi, #{name}"
+def hello_t(array)
+  i=0
+while i<array.lenght
+yield array[1]
+i+=1
 end
- end
+
+def hello_t(array)
+  i = 0
+ 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+end
