c61452c5482db84b391723dcb8763a8fb3ac24af
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-27 02:15:16 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -16,9 +16,18 @@ end
   
 #expect{ hello_t(names){ |name| puts name } }
 
+def hello_t(names)
+  names=[]
+  
 
 
+describe "#hello_t" do
+  let (:names) { ["Tim", "Tom", "Jim"] }
 
-
+  it "calls the block once for each element in the passed-in array" do
+    expect{ hello_t(names){ |name| puts name } }
+      .to output("Tim\nTom\nJim\n")
+      .to_stdout
+  end
 
 
