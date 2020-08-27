ffae8e99f60a675a89911a031b6660d1b19c2364
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-27 02:52:01 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,7 +8,20 @@ array
 end
 
 # call your method here!
-
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
   
   
 #expect{ hello_t(names){ |name| puts name } }
